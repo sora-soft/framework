346ddd5e2c81970d4d27aaf56ea13e5240b71d70
yaya <yaya@xyyaya.com>
2022-03-18 14:29:55 +0800

    feat: 取消tcp传输压缩

--- src/lib/tcp/TCPUtility.ts
+++ src/lib/tcp/TCPUtility.ts
@@ -7,18 +7,18 @@ import {RPCErrorCode} from '../../ErrorCode';
 class TCPUtility {
   static async encodeMessage(packet: IRawNetPacket | IRawReqPacket | IRawResPacket) {
     const data = Buffer.from(JSON.stringify(packet));
-    const deflated = await util.promisify(zlib.deflate)(data);
-    if (deflated.length >= Number.MAX_SAFE_INTEGER) {
-      throw new RPCError(RPCErrorCode.ERR_RPC_PAYLOAD_TOO_LARGE, `ERR_RPC_PAYLOAD_TOO_LARGE`);
-    }
+    // const deflated = await util.promisify(zlib.deflate)(data);
+    // if (deflated.length >= Number.MAX_SAFE_INTEGER) {
+    //   throw new RPCError(RPCErrorCode.ERR_RPC_PAYLOAD_TOO_LARGE, `ERR_RPC_PAYLOAD_TOO_LARGE`);
+    // }
     const header = Buffer.alloc(4);
-    header.writeUInt32BE(deflated.length);
-    return Buffer.concat([header, deflated]);
+    header.writeUInt32BE(data.length);
+    return Buffer.concat([header, data]);
   }
 
   static async decodeMessage(buffer: Buffer) {
-    const inflated = await util.promisify(zlib.inflate)(buffer);
-    return JSON.parse(inflated.toString());
+    // const inflated = await util.promisify(zlib.inflate)(buffer);
+    return JSON.parse(buffer.toString());
   }
 }
 
